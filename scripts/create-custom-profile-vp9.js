2f16fb01a38d3708cd4414a9ce3d5941487e14f9
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-07-12 13:54:05 -0700

    add scripts for custom profiles

---
+++ scripts/create-custom-profile-vp9.js
@@ -0,0 +1,68 @@
+require('dotenv').config()
+const cloudinary = require('cloudinary').v2
+
+cloudinary.api
+  .create_streaming_profile('training_hd_vp9', {
+    display_name: 'Custom training vp9',
+    representations: [
+      {
+        transformation: [
+          {
+            width: 320,
+            height: 240,
+            video_codec: 'vp9',
+            bit_rate: '192k',
+            crop: 'limit'
+          }
+        ]
+      },
+      {
+        transformation: [
+          {
+            width: 480,
+            height: 270,
+            video_codec: 'vp9',
+            bit_rate: '800k',
+            crop: 'limit'
+          }
+        ]
+      },
+      {
+        transformation: [
+          {
+            width: 640,
+            height: 360,
+            video_codec: 'vp9',
+            bit_rate: '2m',
+            crop: 'limit'
+          }
+        ]
+      },
+      {
+        transformation: [
+          {
+            width: 960,
+            height: 540,
+            video_codec: 'vp9',
+            bit_rate: '3500k',
+            crop: 'limit'
+          }
+        ]
+      },
+      {
+        transformation: [
+          {
+            width: 1280,
+            height: 720,
+            video_codec: 'vp9',
+            bit_rate: '5500k',
+            crop: 'limit'
+          }
+        ]
+      }
+    ]
+  })
+  .then(result => {
+    console.log(result)
+  })
+  .catch(error => console.log(error))
