3c71fe5407400f6494b099f6296d90bc36b3d3eb
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-07-13 15:37:28 -0700

    add typewriter effect

--- scripts/listHDProfileTransformations.js
+++ scripts/listHDProfileTransformations.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
+const term = require('terminal-kit').terminal;
 const cloudinary = require('cloudinary').v2
-
 /*
-I'm using the admin api to list all the transformation included in Cloudinary's built in
-hd profile.  New profiles can be created and this profile can server as a template.
+Using the admin api to list all the transformation included in Cloudinary's 
+built in hd profile.  New profiles can be created and this profile can 
+server as a template.
 */
-cloudinary.api
-  .get_streaming_profile('hd')
-  .then(result => console.log(JSON.stringify(result, null, 2)))
-  .catch(error => console.log.log(error))
+const listHDProfile = async () => {
+  const result = await cloudinary.api.get_streaming_profile('hd')
+  term.slowTyping(JSON.stringify(result, null, 2)+'\n',{delay:15})
+}
+listHDProfile()
